4ece51e884f817d1c4a355a788aa3c84aa7177dd
Hyoung <hyoung@techempower.com>
2020-09-15 17:25:20 -0700

    Added copyright and license notices to each file

--- iOS/VeggieBook/VeggieBook/UINavigationControllerExtension.swift
+++ iOS/VeggieBook/VeggieBook/UINavigationControllerExtension.swift
@@ -3,8 +3,20 @@
 //  VeggieBook
 //
 //  Created by Matthew Flickner on 9/8/17.
-//  Copyright © 2017 Technical Empowerment, Inc. All rights reserved.
+//  Copyright © 2020 Quick Help For Meals, LLC. All rights reserved.
 //
+//  This file is part of VeggieBook.
+//
+//  VeggieBook is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, version 3 of the license only.
+//
+//  VeggieBook is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or fitness for a particular purpose. See the
+//  GNU General Public License for more details.
+//
+
 
 import UIKit
 
